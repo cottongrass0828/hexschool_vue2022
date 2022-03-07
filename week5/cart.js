f4584d795ff303972b6670eb1788e4ae404e648e
cottongrass0828 <inoriro.ame@gmail.com>
2022-03-07 21:08:23 +0800

    Vue 第五週作業_第一次繳交

---
+++ week5/cart.js
@@ -0,0 +1,38 @@
+const url = 'https://vue3-course-api.hexschool.io/v2';
+const path = 'piggystore';
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+});
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+const app = Vue.createApp({
+    data() {
+        return {
+            user: {}
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log('送出表單！');
+        },
+        isPhone(value) {
+            const phoneNumber = /^(09)[0-9]{8}$/
+            return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+        }
+    },
+    mounted() {
+    }
+});
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
+
+app.mount('#app');
